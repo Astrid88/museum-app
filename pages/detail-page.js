cae77b4b482b61679d5865d71c9f8d77c5c3d3a2
Astrid88 <giu.munari@gmail.com>
2019-05-30 11:15:05 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -17,5 +17,25 @@ function submitComment() {
     // append children to comment
     comment.appendChild(h3)
     comment.appendChild(p)
+    // append comment to the comment section
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+    
+    /*
+    instead of changing all the values one by one of the form, we could use a loop
+    
+    const listOfInputsValue = [name, msg]
+    // reset form values
+    for (let i = 0; i< listOfInputsValue.length; i++){
+        listOfInputsValue[i] = null
+    }
+    I have grouped the values of the input of the form in an array
+    and then changed them. for this form it is not really needed, 
+    but in case we want to add more input fields (eg. 10), it will be easier
+    to mantain it. we could use the same way also for appending comments to the comment section.  
+    */
+    // reset form values
+    inputField.value = null
+    textArea.value = null
     console.log(comment)
 }
